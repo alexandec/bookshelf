7691b5b957f6892395e402662115405c67bb1c35
Chris Alexander <chris.alexander@adhocteam.us>
2021-12-01 18:18:04 -0700

    Unit testing

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,6 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
 
+test('formatDate formats the date to look nice', () => {
+  const date = new Date('December 12, 2001')
+  expect(formatDate(date)).toBe('Dec 01')
+})
